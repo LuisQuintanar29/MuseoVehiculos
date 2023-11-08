81a061093412b28e6d9ee7b77c930b066ba44552
Luis Quintanar <luisenriqueqramirez@gmail.com>
2023-11-08 00:48:52 -0600

    Update MuseoVehiculos.cpp

--- MuseoVehiculos/MuseoVehiculos/MuseoVehiculos/MuseoVehiculos.cpp
+++ MuseoVehiculos/MuseoVehiculos/MuseoVehiculos/MuseoVehiculos.cpp
@@ -350,7 +350,6 @@ void my_input(GLFWwindow* window, int key, int scancode, int action, int mode)
 
 
 
-
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
